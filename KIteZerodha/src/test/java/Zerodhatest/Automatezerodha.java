119b0deb8c438c4efa7b82b123228adf4a1eec22
Mandar976 <mandarphapale@gmail.com>
2022-01-20 09:22:30 +0530

    new stock

--- KIteZerodha/src/test/java/Zerodhatest/Automatezerodha.java
+++ KIteZerodha/src/test/java/Zerodhatest/Automatezerodha.java
@@ -34,7 +34,7 @@ public class Automatezerodha {
 		pom.enterpin("866918");
 		pom.clickoncontinue();
 		Thread.sleep(5000);
-		pom.enterstockname("TCS");
+		pom.enterstockname("WIPRO");
 		pom.clickonstock();
 	}
 	@AfterClass
